cb5b8b07c9be02b08c3cfab4016b6cc1832b442a
audahadi <wan.audahadi@gmail.com>
2017-05-29 23:52:46 +0800

    Revert "posix_acl : Clear SGID bit when setting file permissions"
    This reverts commit d21ee9eadeac692fbd86846787fa90b886413a91.

--- fs/xfs/xfs_acl.c
+++ fs/xfs/xfs_acl.c
@@ -388,18 +388,10 @@ xfs_xattr_acl_set(struct dentry *dentry, const char *name,
 		goto out_release;
 
 	if (type == ACL_TYPE_ACCESS) {
-		umode_t mode;
-
+		umode_t mode = inode->i_mode;
 		error = posix_acl_update_mode(inode, &mode, &acl);
-
-		if (error <= 0) {
-			posix_acl_release(acl);
-			acl = NULL;
-
-			if (error < 0)
-				return error;
-		}
-
+		if (error)
+			goto out_release;
 		error = xfs_set_mode(inode, mode);
 		if (error)
 			goto out_release;
